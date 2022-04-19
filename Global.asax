da91225cc655e8b2e59a16fba051561b589bb55e
OswaKanguroo <oswaldovaladez@kanguroo.io>
2022-04-18 21:28:24 -0500

    Agregar archivos de proyecto.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppConversion.MvcApplication" Language="C#" %>
---
+++ Models/Convertidor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppConversion.Models
+{
+    public class Convertidor
+    {
+        public int Id { get; set; }
+        public string Num { get; set; }
+        public string NumeroString { get; set; }
+    }
+    public class NumberContext : DbContext
+    {
+        public DbSet<Convertidor> Numero { get; set; }
+    }
+}
